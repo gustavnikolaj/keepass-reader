f2934328cae34c55ea549cd1c0f46d8876b0ddeb
Gustav Nikolaj Olsen <gno@one.com>
2015-09-13 01:31:51 +0200

    add styling to PasswordItems

--- app/components/PasswordItem.jsx
+++ app/components/PasswordItem.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
+import Radium from 'radium'
 
-export default class PasswordItem extends Component {
+class PasswordItem extends Component {
   render () {
     const { title, uuid, isSelected } = this.props
     let selectedStyle = {}
@@ -8,11 +9,11 @@ export default class PasswordItem extends Component {
       selectedStyle.border = '3px solid blue'
     }
     return (
-      <div className='passwordItem' style={ selectedStyle }>
-        <h2 className='passwordItem title'>
-          {title}
-        </h2>
-        <i>{uuid}</i>
+      <div style={[ styles.base, isSelected && styles.selected ]}>
+        <div style={ styles.title }>{title}</div>
+        <div style={ styles.detail }>
+          <i>{uuid}</i>
+        </div>
       </div>
     )
   }
@@ -23,3 +24,29 @@ PasswordItem.propTypes = {
   title: PropTypes.string,
   isSelected: PropTypes.bool
 }
+
+var styles = {
+  selected: {
+    background: '#eef',
+    border: '1px solid #369'
+  },
+  base: {
+    background: '#fafafa',
+    borderRadius: 5,
+    border: '1px solid #ddd',
+    lineHeight: 1.5,
+    paddingTop: 5,
+    paddingRight: 10,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    margin: 5
+  },
+  title: {
+    fontWeight: 'bold'
+  },
+  detail: {
+    fontSize: '0.75em'
+  }
+}
+
+export default Radium(PasswordItem)
