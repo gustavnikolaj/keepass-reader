8a2817cc80f8dcf049d5e192734057dba580cf7b
Gustav Nikolaj Olsen <gno@one.com>
2015-09-12 17:52:37 +0200

    add ipcBus and refactor into handlers

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var menubar = require('menubar')
 var globalShortcut = require('global-shortcut')
-var ipc = require('ipc')
 var path = require('path')
-var dialog = require('dialog')
 var KeepassClient = require('./lib/keepassClient')
 var AppDataClient = require('./lib/appDataClient')
+var IpcBus = require('./lib/ipcBus')
 
 require('electron-debug')()
 require('crash-reporter').start()
@@ -39,11 +38,29 @@ if (WINDOW_MODE_MENUBAR) {
   }
 }
 
+var ipcBus = new IpcBus(require('ipc'))
 var keepassClient = new KeepassClient()
 var appDataClient = new AppDataClient({
   appDataPath: mb.app.getPath('appData'),
   appName: 'keepass-menubar'
 })
+
+ipcBus
+  .use('password', require('./lib/handlers/password')({
+    keepassClient: keepassClient
+  }))
+  .use('passwordList', require('./lib/handlers/passwordList')({
+    keepassClient: keepassClient
+  }))
+  .use('path', require('./lib/handlers/path')({
+    keepassClient: keepassClient,
+    appDataClient: appDataClient
+  }))
+  .use('pathDialog', require('./lib/handlers/pathDialog')({
+    dialog: require('dialog'),
+    keepassClient: keepassClient,
+    appDataClient: appDataClient
+  }))
 
 function registerShortcut () {
   function openApplication () {
@@ -80,72 +97,4 @@ mb.app.on('ready', function () {
 
 mb.app.on('will-quit', function () {
   unregisterShortcut()
-})
-
-ipc.on('passwordRequest', function (event, uuid) {
-  keepassClient.getPassword(uuid, function (err, password) {
-    if (err) {
-      return event.sender.send('passwordResponse', {
-        error: 'LOCKED_DATABASE'
-      })
-    }
-
-    return event.sender.send('passwordResponse', { password })
-  })
-})
-
-ipc.on('passwordListRequest', function (event, masterKey) {
-  if (masterKey) {
-    keepassClient.masterKey(masterKey)
-  }
-  keepassClient.getList(function (err, passwords) {
-    if (err) {
-      console.log(err)
-      return event.sender.send('passwordListResponse', {
-        error: 'LOCKED_DATABASE'
-      })
-    }
-
-    return event.sender.send('passwordListResponse', {
-      passwords
-    })
-  })
-})
-
-ipc.on('pathRequest', function (event) {
-  appDataClient.get('keepassDatabasePath', function (err, databasePath) {
-    if (err) {
-      keepassClient.path(null)
-      return event.sender.send('pathResponse', { path: '' })
-    }
-    if (databasePath !== keepassClient.path()) {
-      keepassClient.path(databasePath)
-    }
-    return event.sender.send('pathResponse', { path: databasePath })
-  })
-})
-
-ipc.on('pathDialogRequest', function (event) {
-  var path = dialog.showOpenDialog({
-    defaultPath: process.env.HOME,
-    title: 'Select your kdbx database',
-    filters: [
-      {
-        name: 'Keepass 2 Databases (.kdbx)',
-        extensions: [ 'kdbx' ]
-      }
-    ],
-    properties: [ 'openFile' ]
-  })
-
-  if (path && path.length === 1) {
-    path = path[0]
-  } else {
-    path = null
-  }
-
-  appDataClient.set('keepassDatabasePath', path, function () {
-    keepassClient.path(path)
-    return event.sender.send('pathDialogResponse', { path: path })
-  })
 })
