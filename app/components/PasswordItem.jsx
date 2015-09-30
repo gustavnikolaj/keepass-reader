f86b41184669d563d104da33f94f37d023cbf83d
Gustav Nikolaj Olsen <gno@one.com>
2015-09-30 18:23:24 +0200

    PasswordItem: replace Radium with CSS modules

--- app/components/PasswordItem.jsx
+++ app/components/PasswordItem.jsx
@@ -1,29 +1,30 @@
 import React, { Component, PropTypes } from 'react'
-import Radium from 'radium'
+import styles from './PasswordItem.less'
 
 class PasswordItem extends Component {
   render () {
     const { title, isSelected, username, url } = this.props
-    let selectedStyle = {}
-    if (isSelected) {
-      selectedStyle.border = '3px solid blue'
-    }
 
     let detailItems = [
       { label: 'Username', data: username },
       { label: 'URL', data: url }
     ].filter(entry => { return entry.data !== '' }).map(entry => {
       return (
-        <div style={ styles.detailItem }>
+        <div className={ styles.detailItem }>
           <b>{entry.label}:</b> {entry.data}
         </div>
       )
     })
 
+    let classNames = [styles.base]
+    if (isSelected) {
+      classNames.push(styles.selected)
+    }
+
     return (
-      <div style={[ styles.base, isSelected && styles.selected ]}>
-        <div style={ styles.title }>{title}</div>
-        <div style={ styles.detail }>{detailItems}</div>
+      <div className={ classNames.join(' ') }>
+        <div className={ styles.title }>{title}</div>
+        <div className={ styles.detail }>{detailItems}</div>
       </div>
     )
   }
@@ -36,32 +37,4 @@ PasswordItem.propTypes = {
   isSelected: PropTypes.bool
 }
 
-var styles = {
-  selected: {
-    background: '#eef',
-    border: '1px solid #369'
-  },
-  base: {
-    background: '#fafafa',
-    borderRadius: 5,
-    border: '1px solid #ddd',
-    lineHeight: 1.5,
-    paddingTop: 5,
-    paddingRight: 10,
-    paddingBottom: 5,
-    paddingLeft: 10,
-    margin: 10
-  },
-  title: {
-    fontWeight: 'bold'
-  },
-  detail: {
-    fontSize: '0.75em',
-    display: 'flex'
-  },
-  detailItem: {
-    flex: 1
-  }
-}
-
-export default Radium(PasswordItem)
+export default PasswordItem
