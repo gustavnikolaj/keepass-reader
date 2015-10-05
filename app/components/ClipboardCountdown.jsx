42658367e4559b70969432697b5bb4510dc1ff1e
Gustav Nikolaj Olsen <gno@one.com>
2015-10-05 22:24:51 +0200

    ClipboardCountdown: replace Radium with CSS Modules

--- app/components/ClipboardCountdown.jsx
+++ app/components/ClipboardCountdown.jsx
@@ -1,14 +1,11 @@
 import React, { Component, PropTypes } from 'react'
-import Radium from 'radium'
+import styles from './ClipboardCountdown.less'
 
 class ClipboardCountdown extends Component {
   constructor (props) {
     super(props)
     this.interval = false
-
-    this.setState({
-      remainingPercentage: 0
-    })
+    this.state = { percentage: 0 }
 
     this.componentWillReceiveProps(props)
     this.tick = this.tick.bind(this)
@@ -44,8 +41,9 @@ class ClipboardCountdown extends Component {
         right: '' + (100 - percentage) + '%'
       }
       return (
-        <div style={ styles.barBase }>
-          <div style={[ styles.barBase, styles.barFront, style ]}></div>
+        <div className={ styles.barBase }>
+          <div style={ style }
+               className={ [styles.barBase, styles.barFront].join(' ') }></div>
         </div>
       )
     } else {
@@ -58,21 +56,4 @@ ClipboardCountdown.propTypes = {
   clipboardTimer: PropTypes.obj
 }
 
-var styles = {
-  barBase: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 4,
-    background: '#fafafa',
-    zIndex: 1000
-  },
-  barFront: {
-    zIndex: 1001,
-    background: '#369',
-    'transition': '200ms ease-in right'
-  }
-}
-
-export default Radium(ClipboardCountdown)
+export default ClipboardCountdown
